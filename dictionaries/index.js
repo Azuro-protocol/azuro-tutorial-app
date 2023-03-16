781bd1cc6568eee361393b403ed3ee4482c63303
Pavel Ivanov <grammka@gmail.com>
2023-03-16 20:55:48 +0300

    add dictionaries

---
+++ dictionaries/index.js
@@ -0,0 +1,25 @@
+import outcomes from './outcomes'
+import sports from './sports'
+import markets from './markets'
+import gamePeriods from './gamePeriods'
+import gameTypes from './gameTypes'
+import gameVarieties from './gameVarieties'
+import teamPlayers from './teamPlayers'
+import points from './points'
+import selections from './selections'
+import marketNames from './marketNames'
+import marketDescriptions from './marketDescriptions'
+
+export default {
+  outcomes,
+  sports,
+  markets,
+  gamePeriods,
+  gameTypes,
+  gameVarieties,
+  teamPlayers,
+  points,
+  selections,
+  marketNames,
+  marketDescriptions
+}  
