1864c545f3996f0729cc74346bbfc14f0d8b81a0
Pavel Ivanov <grammka@gmail.com>
2023-03-16 22:57:36 +0300

    place a first bet

---
+++ components/PlaceBetModal.js
@@ -0,0 +1,117 @@
+import dayjs from 'dayjs'
+import { getMarketName } from '@azuro-org/dictionaries'
+import dictionaries from '@/dictionaries'
+import usePlaceBet from '@/hooks/usePlaceBet'
+
+const GameInfo = ({ game }) => (
+  <div className="py-4 px-6 rounded-b-xl shadow-xl">
+    <div className="flex justify-between text-md">
+      <span>{game.sport.name}</span>
+      <span>{dayjs(game.startsAt * 1000).format('DD MMM HH:mm')}</span>
+    </div>
+    <div className="mt-1 text-md text-gray-400">
+      {game.league.country.name} &middot; {game.league.name}
+    </div>
+    <div className="mt-3 space-y-2">
+      {
+        game.participants.map(({ image, name }) => (
+          <div key={name} className="flex items-center">
+            <div className="flex items-center justify-center w-8 h-8 mr-2 border border-gray-300 rounded-full">
+              <img className="w-4 h-4" src={image} alt={name} />
+            </div>
+            <span className="text-md">{name}</span>
+          </div>
+        ))
+      }
+    </div>
+  </div>
+)
+
+const OutcomeInfo = ({ outcome }) => {
+  const marketName = getMarketName(outcome.outcomeId, dictionaries)
+
+  return (
+    <div className="grid grid-cols-[auto_1fr] gap-y-3 mt-2 text-md">
+      <span className="text-gray-400">Market</span>
+      <span className="text-right font-semibold">{marketName}</span>
+      <span className="text-gray-400">Selection</span>
+      <span className="text-right font-semibold">{outcome.selectionName}</span>
+      <span className="text-gray-400">Odds</span>
+      <span className="text-right font-semibold">{outcome.odds}</span>
+    </div>
+  )
+}
+
+const PlaceBetModal = ({ game, outcome, closeModal }) => {
+  const {
+    isRightChain,
+    balance,
+    amount,
+    setAmount,
+    isAllowanceFetching,
+    isApproveRequired,
+    approve,
+    isApproving,
+    placeBet,
+  } = usePlaceBet({ outcome, onBetPlace: closeModal })
+
+  const amountsNode = (
+    <div className="mt-4 pt-4 border-t border-gray-300 space-y-2">
+      <div className="flex items-center justify-between">
+        <span className="text-md text-gray-400">Wallet balance</span>
+        <span className="text-md font-semibold">-</span>
+      </div>
+      <div className="flex items-center justify-between">
+        <span className="text-md text-gray-400">Bet amount</span>
+        <input
+          className="w-[121px] py-2 px-4 border border-gray-400 text-md text-right font-semibold rounded-md"
+          type="number"
+          placeholder="Bet amount"
+          value={amount}
+          onChange={(event) => setAmount(event.target.value)}
+        />
+      </div>
+    </div>
+  )
+
+  const button = !isRightChain ? (
+    <div className="mt-6 py-2.5 text-center bg-red-200 rounded-md">
+      Switch network in your wallet to <b>Polygon</b>
+    </div>
+  ) : (
+    <button
+      className="button w-full mt-6 py-2.5 text-center"
+      disabled={isAllowanceFetching || isApproving}
+      onClick={isApproveRequired ? approve : placeBet}
+    >
+      {
+        isApproveRequired ? (
+          isApproving ? 'Approving...' : 'Approve'
+        ) : (
+          'Place bet'
+        )
+      }
+    </button>
+  )
+
+  return (
+    <div
+      className="fixed top-0 left-0 z-50 w-full h-full flex items-center justify-center bg-black bg-opacity-20"
+      onClick={closeModal}
+    >
+      <div
+        className="w-[400px] bg-white overflow-hidden rounded-xl shadow-2xl"
+        onClick={(event) => event.stopPropagation()}
+      >
+        <GameInfo game={game} />
+        <div className="pt-4 px-6 pb-6">
+          <OutcomeInfo outcome={outcome} />
+          {amountsNode}
+          {button}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default PlaceBetModal
--- pages/games/[id].js
+++ pages/games/[id].js
@@ -1,5 +1,7 @@
 import dayjs from 'dayjs'
 import useSportEvent from '@/hooks/useSportEvent'
+import { useState } from 'react'
+import PlaceBetModal from '@/components/PlaceBetModal'
 
 const ParticipantLogo = ({ image, name }) => (
   <div className="flex flex-col items-center">
@@ -28,39 +30,63 @@ const GameInfo = ({ sport, league, participants, startsAt }) => (
   </div>
 )
 
-const Markets = ({ markets }) => (
-  <div className="max-w-[600px] mx-auto mt-12 space-y-6">
-    {
-      markets.map(({ marketName, outcomes: row }) => (
-        <div key={marketName} className="">
-          <div className="mb-2 font-semibold">{marketName}</div>
-          <div className="space-y-1">
-            {
-              row.map((outcomes, index) => (
-                <div key={index} className="flex justify-between">
-                  <div className="flex gap-1 w-full">
-                    {
-                      outcomes.map(({ selectionName, odds }) => (
-                        <div
-                          key={selectionName}
-                          className="flex justify-between py-2 px-3 bg-gray-200 rounded-md cursor-pointer hover:bg-gray-300 transition"
-                          style={{ width: `calc(100% / ${outcomes.length})` }}
-                        >
-                          <span className="text-gray-500">{selectionName}</span>
-                          <span className="font-medium">{parseFloat(odds).toFixed(2)}</span>
-                        </div>
-                      ))
-                    }
-                  </div>
-                </div>
-              ))
-            }
-          </div>
-        </div>
-      ))
-    }
-  </div>
-)
+const Markets = ({ game, markets }) => {
+  const [ selectedOutcome, setSelectedOutcome ] = useState(null)
+
+  const handleOutcomeClick = (outcome) => {
+    setSelectedOutcome(outcome)
+  }
+
+  const handleModalClose = () => {
+    setSelectedOutcome(null)
+  }
+
+  return (
+    <>
+      <div className="max-w-[600px] mx-auto mt-12 space-y-6">
+        {
+          markets.map(({ marketName, outcomes: row }) => (
+            <div key={marketName} className="">
+              <div className="mb-2 font-semibold">{marketName}</div>
+              <div className="space-y-1">
+                {
+                  row.map((outcomes, index) => (
+                    <div key={index} className="flex justify-between">
+                      <div className="flex gap-1 w-full">
+                        {
+                          outcomes.map((outcome) => (
+                            <div
+                              key={outcome.selectionName}
+                              className="flex justify-between py-2 px-3 bg-gray-200 rounded-md cursor-pointer hover:bg-gray-300 transition"
+                              style={{ width: `calc(100% / ${outcomes.length})` }}
+                              onClick={() => handleOutcomeClick(outcome)}
+                            >
+                              <span className="text-gray-500">{outcome.selectionName}</span>
+                              <span className="font-medium">{parseFloat(outcome.odds).toFixed(2)}</span>
+                            </div>
+                          ))
+                        }
+                      </div>
+                    </div>
+                  ))
+                }
+              </div>
+            </div>
+          ))
+        }
+      </div>
+      {
+        Boolean(selectedOutcome) && (
+          <PlaceBetModal
+            game={game}
+            outcome={selectedOutcome}
+            closeModal={handleModalClose}
+          />
+        )
+      }
+    </>
+  )
+}
 
 export default function Game() {
   const { loading, game, markets } = useSportEvent()
@@ -72,7 +98,7 @@ export default function Game() {
   return (
     <main>
       <GameInfo {...game} />
-      <Markets markets={markets} />
+      <Markets game={game} markets={markets} />
     </main>
   )
 }
